722058d934726e39345a5f2f146456afb4e32378
PierreThb <thube.pierre@gmail.com>
2015-11-30 10:46:03 +0000

    add commentary inside code

--- main.c
+++ main.c
@@ -6,17 +6,18 @@
 int main(int argc, char *argv[])
 {
 
-Planet Q,K,J;
-int number_planet = 3;
-float forces[number_planet][number_planet+1];
-float pos[number_planet][2];
+Planet Q,K,J; //declare 3 struct Planet
+int number_planet = 3; // I try with 3 planet to begin
+float forces[number_planet][number_planet+1]; //array which contained all forces betwin planets
+float pos[number_planet][2]; //array which contained all positions of all planets
 float x_diff, y_diff, dist, dist_cubed = 0;
 
-
+//initialize PLanet
 Q.masse=5; Q.pos_x=5.3; Q.pos_y=12.2; Q.velocity=112;
 K.masse=8; K.pos_x=7.2; K.pos_y=5.6; Q.velocity=135;
 J.masse=3; J.pos_x=10.3; J.pos_y=8.1; Q.velocity=88;
 
+//initialize posotion of all planets
 pos[0][0] = Q.pos_x; pos[0][1]=Q.pos_y;
 pos[1][0]= K.pos_x; pos[1][1]=K.pos_y;
 pos[2][0]= J.pos_x; pos[2][1] = J.pos_y;
